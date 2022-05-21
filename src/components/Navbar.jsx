47f68a209e6b9fd485d90526d026eaaec99b42d6
unknown <nafis_fuad1234@yahoo.com>
2022-05-21 17:12:45 +0600

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+
 import {BsPerson} from 'react-icons/bs'
 import {BiSearch} from 'react-icons/bi'
-import {AiOutLineClose} from 'react-icons/ai'
+import {AiOutlineClose} from 'react-icons/ai'
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
-import {FaFacebook, FaPinterest , FaTwitter , FaYoutube} from 'react-icons/fa'
+import {FaFacebook, FaInstagram, FaPinterest , FaTwitter , FaYoutube} from 'react-icons/fa'
+import {React, useState} from 'react'
 
 function Navbar() {
+  const [hamButton, triggerhamButton] = useState(false);
+  const handleNav = () =>{
+    triggerhamButton(!hamButton)
+  };
   return (
     <div className='flex justify-between items-center h-20 px-4 '>
       <div >
@@ -24,27 +29,29 @@ function Navbar() {
       </div>
 
       {/* Hamburger */}
-      <div className='md:hidden'>
-        <HiOutlineMenuAlt4 size= {30}/>
+      <div onClick={handleNav} className='md:hidden z-10'>
+        { hamButton ? <AiOutlineClose className='text-black' size={30}/> : <HiOutlineMenuAlt4 size={30} />}
+        
       </div>
        {/*Mobile menu dropdown */}
-      <div className='absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'>
+      <div onClick={handleNav} className={hamButton ? 'absolute top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
       <ul>
         <h1>BEACHES</h1>
-        <li>Home</li>
-        <li>Destinations</li>
-        <li>Travel</li>
-        <li>View</li>
-        <li>Book</li>
-        <div>
-          <button>Search</button>
+        <li className='border-b'>Home</li>
+        <li className='border-b'>Destinations</li>
+        <li className='border-b'>Travel</li>
+        <li className='border-b'>View</li>
+        <li className='border-b'>Book</li>
+        <div className=' flex flex-col my-5 pr-4'>
+          <button className='mb-4'>Search</button>
           <button>Account</button>
         </div>
-        <div className=''>
+        <div className='flex flex-row justify-between mt-4'>
           <FaFacebook className ='icon'  />
           <FaTwitter  className ='icon' />
           <FaYoutube  className ='icon' />
           <FaPinterest  className ='icon' />
+          <FaInstagram className='icon' />
           
         </div>
       </ul>

57e76da9366773894a234066227aca82778f5cfc
unknown <nafis_fuad1234@yahoo.com>
2022-05-23 10:26:31 +0600

    First 2 sections complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import React from 'react';
+import BackGround from './components/BackGround';
+import Destinations from './components/Destinations';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
 
 function App() {
   return (
     <div >
-       <Navbar/>   
+      <Navbar/>  
+      <BackGround/>
+      <Destinations/>
+      <Search/>
+      
     </div>
+    
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,15 @@ import {React, useState} from 'react'
 
 function Navbar() {
   const [hamButton, triggerhamButton] = useState(false);
+  const [logo , setLogo] = useState(false);
   const handleNav = () =>{
-    triggerhamButton(!hamButton)
+    triggerhamButton(!hamButton);
+    setLogo(!logo);
   };
   return (
-    <div className='flex justify-between items-center h-20 px-4 '>
+    <div className='flex w-full justify-between items-center h-20 md:px-4 absolute z-10 text-white'>
       <div >
-        <h1>BEACHES</h1>
+        <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES</h1>
       </div>
       <ul className='hidden md:flex'>
         <li>Home</li>
@@ -23,7 +25,7 @@ function Navbar() {
         <li>View</li>
         <li>Book</li>
       </ul>
-      <div className='hidden md:flex'>  
+      <div className='hidden md:flex mr-5'>  
       <BiSearch size= {20}/>
       <BsPerson size= {20}/>
       </div>
@@ -33,8 +35,9 @@ function Navbar() {
         { hamButton ? <AiOutlineClose className='text-black' size={30}/> : <HiOutlineMenuAlt4 size={30} />}
         
       </div>
+
        {/*Mobile menu dropdown */}
-      <div onClick={handleNav} className={hamButton ? 'absolute top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+      <div onClick={handleNav} className={hamButton ? 'absolute text-black top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
       <ul>
         <h1>BEACHES</h1>
         <li className='border-b'>Home</li>
@@ -42,20 +45,24 @@ function Navbar() {
         <li className='border-b'>Travel</li>
         <li className='border-b'>View</li>
         <li className='border-b'>Book</li>
+
         <div className=' flex flex-col my-5 pr-4'>
           <button className='mb-4'>Search</button>
           <button>Account</button>
         </div>
-        <div className='flex flex-row justify-between mt-4'>
+
+        <div className='flex flex-row justify-between mt-4 pr-4'>
           <FaFacebook className ='icon'  />
           <FaTwitter  className ='icon' />
           <FaYoutube  className ='icon' />
           <FaPinterest  className ='icon' />
-          <FaInstagram className='icon' />
-          
+          <FaInstagram className='icon' />     
         </div>
+
       </ul>
+
       </div>
+
     </div>
   )
 }
